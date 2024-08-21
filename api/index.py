a4b58c57372276543e16c1be77e481693bd23a21
thealeef <aleef1993@gmail.com>
2024-08-20 23:53:05 -0300

    Testando os MOCKS

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
@@ -8,6 +11,14 @@ def Inicio():
     return "API Funcionando"
 
 
-@app.route("/api")
-def API():
-    return "About"
+@app.route("/funcionarios")
+def funcionarios():
+    # MOKANDO OS DADOS
+    listaFuncionarios = [
+        {"id": 1, "nomeCompleto": "Teste A", "nomeMae": "Mae Teste A"},
+        {"id": 2, "nomeCompleto": "Teste B", "nomeMae": "Mae Teste B"},
+        {"id": 3, "nomeCompleto": "Teste C", "nomeMae": "Mae Teste C"},
+        {"id": 4, "nomeCompleto": "Teste D", "nomeMae": "Mae Teste D"},
+    ]
+
+    return jsonify(listaFuncionarios)
