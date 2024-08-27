85e56e30da5f953c72cbca8187ad76d125f3cdf0
thealeef <aleef1993@gmail.com>
2024-08-27 00:14:42 -0300

    MSG

--- api/index.py
+++ api/index.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 ## OBS: Não colocar o CORs ele buga o vercel
 
+
+@app.route("/", methods=["GET"])
+def chama_funcionarios():
+    return jsonify("API Rodando")
+
+
 contador_id = 1
 
 
